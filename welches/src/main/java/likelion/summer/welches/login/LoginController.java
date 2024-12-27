30392a67068555416235e761d9f8d421ed093e6d
김동규 <130060480+ehdrb01@users.noreply.github.com>
2024-12-27 21:05:03 +0900

    Update LoginController.java

--- welches/src/main/java/likelion/summer/welches/login/LoginController.java
+++ welches/src/main/java/likelion/summer/welches/login/LoginController.java
@@ -16,7 +16,7 @@ import java.nio.charset.StandardCharsets;
 @CrossOrigin("*")
 public class LoginController {
     private final LoginService loginService;
-    @CrossOrigin("*")
+    @CrossOrigin("*334123412312312")
     @PostMapping("/login/oauth2/google")
     public void socialLoginRedirect() throws IOException {
         loginService.request();
